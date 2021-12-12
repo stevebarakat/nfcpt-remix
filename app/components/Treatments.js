ca0b000f75a014a2b2adbf9edde1d2a5ea2eacb8
Steve <stevebarakat@gmail.com>
2021-12-12 05:00:45 -0500

    check out

--- app/components/Treatments.js
+++ app/components/Treatments.js
@@ -1,78 +1,22 @@
 import React from "react";
-import { useLoaderData } from "remix";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import SwiperCore, { Autoplay, Pagination } from "swiper";
-
-// import "swiper/css";
-// import "swiper/css/pagination";
+import chiro from "../images/chiro.svg";
+import figure from "../images/figure2.svg";
 
 const Treatments = ({ footerData }) => {
+  const { disorders } = footerData;
   return (
     <div className="treatmentsWrap">
       <div className="container">
         <h2 className="h2">Injuries & Conditions We Treat</h2>
-        {/* <Swiper
-          speed={750}
-          spaceBetween={0}
-          slidesPerView={4}
-          slidesPerGroup={1}
-          loop={true}
-          loopFillGroupWithBlank={true}
-          centeredSlides={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: true,
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          breakpoints={{
-            0: {
-              slidesPerView: 1,
-              spaceBetween: 10,
-            },
-            570: {
-              slidesPerView: 2,
-              spaceBetween: 20,
-            },
-            690: {
-              slidesPerView: 3,
-              spaceBetween: 30,
-            },
-            980: {
-              slidesPerView: 4,
-              spaceBetween: 40,
-            },
-            1200: {
-              slidesPerView: 5,
-              spaceBetween: 50,
-            },
-          }}
-        >
-          {disorders.map((disorder, i) => {
-            return (
-              <SwiperSlide key={i}>
-                <div className={styles.card}>
-                  <span className={styles.cardHeader}>
-                    {disorder.disorder.disorderName}
-                  </span>
-                  <div
-                    style={{
-                      position: "relative",
-                      width: "200px",
-                      height: "200px",
-                    }}
-                  >
-                    <img
-                      src={disorder.disorder.disorderImage.sourceUrl}
-                      alt={disorder.disorder.disorderName}
-                    />
-                  </div>
-                </div>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper> */}
+        <div className="disordersWrap">
+          <div>Whiplash</div>
+          <div>Tendonitis & Bursitis</div>
+          <div>Migraine Headaches</div>
+          <img src={figure} alt="disorders" />
+          <div>Pinched Nerve</div>
+          <div>Hip & Joint Pain</div>
+          <div>Sciatica & Herniated Disk</div>
+        </div>
       </div>
     </div>
   );
