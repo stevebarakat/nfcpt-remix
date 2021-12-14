bd74af76f45884e36051dd260f57e65f24be694f
Steve <stevebarakat@gmail.com>
2021-12-13 19:05:01 -0500

    check in

--- app/components/Treatments.js
+++ app/components/Treatments.js
@@ -8,14 +8,100 @@ const Treatments = ({ footerData }) => {
     <div className="treatmentsWrap">
       <div className="container">
         <h2 className="h2">Injuries & Conditions We Treat</h2>
-        <div className="disordersWrap">
-          <div>Whiplash</div>
-          <div>Tendonitis & Bursitis</div>
-          <div>Migraine Headaches</div>
-          <img src={figure} alt="disorders" />
-          <div>Pinched Nerve</div>
-          <div>Hip & Joint Pain</div>
-          <div>Sciatica & Herniated Disk</div>
+
+        <div className="infographic">
+          <div className="items-left">
+            <div className="item-left" style={{ top: "6.5rem" }}>
+              <a href="https://github.com/">To a link to a to link to Github</a>
+              <div
+                className="line-left"
+                style={{ transform: "rotate(30deg)" }}
+              ></div>
+            </div>
+            <div className="item-left" style={{ top: "14rem" }}>
+              <a href="https://github.com/">Here's a link to Github</a>
+              <div
+                className="line-left"
+                style={{ transform: "rotate(22deg)" }}
+              ></div>
+            </div>
+            <div className="item-left" style={{ top: "20rem" }}>
+              <a href="https://github.com/">Another Link To Github</a>
+              <div
+                className="line-left"
+                style={{ transform: "rotate(15deg)" }}
+              ></div>
+            </div>
+            <div className="item-left" style={{ top: "25rem" }}>
+              <a href="https://github.com/">Link to Github</a>
+              <div
+                className="line-left"
+                style={{ transform: "rotate(-15deg)" }}
+              ></div>
+            </div>
+            <div className="item-left" style={{ top: "31rem" }}>
+              <a href="https://github.com/">
+                Here's a link to a link to Github
+              </a>
+              <div
+                className="line-left"
+                style={{ transform: "rotate(-8deg)" }}
+              ></div>
+            </div>
+          </div>
+
+          <img src={figure} alt="alt text here" />
+
+          <div className="items-right">
+            <div className="item-right" style={{ top: "1.5rem" }}>
+              <div
+                className="line-right"
+                style={{
+                  transform: "rotate(-35deg)",
+                  width: "200px",
+                  marginLeft: "-2.3rem",
+                }}
+              ></div>
+              <a href="https://github.com/">Here's a link to Github</a>
+            </div>
+            <div className="item-right" style={{ top: "10.5rem" }}>
+              <div
+                className="line-right"
+                style={{ transform: "rotate(-25deg)" }}
+              ></div>
+              <a href="https://github.com/">Here's a link to Github</a>
+            </div>
+            <div className="item-right" style={{ top: "17rem" }}>
+              <div
+                className="line-right"
+                style={{ transform: "rotate(-15deg)" }}
+              ></div>
+              <a href="https://github.com/">Link to Github</a>
+            </div>
+            <div className="item-right" style={{ top: "23.25rem" }}>
+              <div
+                className="line-right"
+                style={{ transform: "rotate(5deg)" }}
+              ></div>
+              <a href="https://github.com/">To Github</a>
+            </div>
+            <div className="item-right" style={{ top: "28rem" }}>
+              <div
+                className="line-right"
+                style={{ transform: "rotate(12.5deg)" }}
+              ></div>
+              <a href="https://github.com/">
+                Here's a link to a link to a link to Github
+              </a>
+            </div>
+            <div className="item-right" style={{ top: "36rem" }}>
+              <div
+                className="line-right"
+                style={{ transform: "rotate(25deg)" }}
+              ></div>
+              <a href="https://github.com/">Here's a link to Github</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
